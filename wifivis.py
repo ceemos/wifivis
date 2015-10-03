06a49612ef70d4e7fe1cb2391bb382793e838f25
Marcel Schneider <marcel.schneider@student.ethz.ch>
2015-10-03 06:45:53 +0200

    Graph drwaing

--- wifivis.py
+++ wifivis.py
@@ -4,6 +4,50 @@ from flask import Flask
 import json
 
 
+testgraph = {
+    "11:22:33:44:55" : {
+        "weight" : 1.0,
+        "x" : 0.5,
+        "y" : 0.5,
+        "edges" : {
+            "12:34:56:67:90" : 1.0,
+            "aa:bb:cc:dd:ff" : 10.0,
+            "aa:11:bb:22:cc" : 2.0,
+            "ab:bc:cd:de:ef" : 2.0,
+        }
+    },
+    "12:34:56:67:90" : {
+        "weight" : 0.3,
+        "x" : 0.1,
+        "y" : 0.1,
+        "edges" : {
+            "aa:bb:cc:dd:ff" : 10.0,
+        }
+    },
+    "aa:bb:cc:dd:ff" : {
+        "weight" : 0.3,
+        "x" : 0.1,
+        "y" : 0.7,
+        "edges" : {
+        }
+    },
+    "aa:11:bb:22:cc" : {
+        "weight" : 0.5,
+        "x" : 0.7,
+        "y" : 0.8,
+        "edges" : {
+        }
+    },
+    "ab:bc:cd:de:ef": {
+        "weight" : 0.1,
+        "x" : 0.9,
+        "y" : 0.2,
+        "edges" : {
+        }
+    }   
+}   
+    
+
 def frontend(q):
     app = Flask(__name__)
     
@@ -13,10 +57,13 @@ def frontend(q):
         with open("ui.css") as f: css = f.read()
         
         return '''
+        <!DOCTYPE html>
         <html>
             <head>
+                <meta charset="UTF-8">
+                <title>wifivis</title>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-                <style "type=text/css">
+                <style type="text/css">
         ''' +  css + '''       
                 </style>
                 <script type="text/javascript">
@@ -33,8 +80,7 @@ def frontend(q):
     @app.route('/dynamic')
     def ajax():
         update = q.get()
-        print(json.dumps(update))
-        return json.dumps(update)
+        return json.dumps(testgraph)
 
     app.run()
     
